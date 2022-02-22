a4765d3c73469a18c8ce101349d0b277e425f611
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-02-22 20:50:59 +0200

    Initial Commit

---
+++ InstaClone/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by MJ Albdour on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
