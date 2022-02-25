4898b97af2d4d55fb77250d0367f8ae81f65c527
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-02-25 15:03:44 +0300

    Adjusted helper buttons padding, created photo grids

--- InstaClone/Views/PersonalProfileView.swift
+++ InstaClone/Views/PersonalProfileView.swift
@@ -8,27 +8,28 @@
 import SwiftUI
 
 struct PersonalProfileView: View {
+    @State var selection = 0
     var body: some View {
-        VStack(alignment: .leading) {
-            ActivitySection()
-                .padding(.bottom)
-            DashboardSection()
-                .padding(.bottom)
-            MainSection()
-            //                .padding(.bottom)
+        ScrollView {
             VStack(alignment: .leading) {
-                Name()
-                AccountCategory()
-                Bio()
+                ActivitySection()
+                    .padding(.bottom)
+                DashboardSection()
+                    .padding(.bottom)
+                MainSection()
+                VStack(alignment: .leading) {
+                    Name()
+                    AccountCategory()
+                    Bio()
+                }
+                .padding(.bottom, 1)
+                HelpersSection()
+                    .padding(.bottom)
+                HighlightsSection()
+                TabBar(selection: $selection)
+                TabViewGrid(selection: $selection)
             }
-            .padding(.bottom,1)
-            HelpersSection()
-                .padding(.bottom)
-            HighlightsSection()
-                .padding(.bottom)
-            Spacer()
         }
-        .padding()
     }
 }
 
@@ -158,7 +159,7 @@ struct HelpersSection: View {
             }
             .background(.black)
             .foregroundColor(.white)
-            .padding(.vertical, 5)
+            .padding(.vertical, 10.0)
             //                .padding(.horizontal, 1)
             .frame(minWidth: 0, maxWidth: .infinity)
             .border(.white.opacity(0.3), width: 1)
@@ -172,7 +173,7 @@ struct HelpersSection: View {
             }
             .background(.black)
             .foregroundColor(.white)
-            .padding(.vertical, 5)
+            .padding(.vertical, 10)
             //                .padding(.horizontal, 1)
             .frame(minWidth: 0, maxWidth: .infinity)
             .border(.white.opacity(0.3), width: 1)
@@ -186,7 +187,7 @@ struct HelpersSection: View {
             }
             .background(.black)
             .foregroundColor(.white)
-            .padding(.vertical, 5)
+            .padding(.vertical, 10)
             //                .padding(.horizontal, 1)
             .frame(minWidth: 0, maxWidth: .infinity)
             .border(.white.opacity(0.3), width: 1)
@@ -202,11 +203,11 @@ struct HighlightsSection: View {
                 Circle()
                     .stroke()
                     .foregroundColor(.gray)
-                    .frame(width: 70, height: 70)
+                    .frame(width: 60, height: 60)
                     .overlay(Image(systemName: "globe.americas")
                                 .resizable()
                                 .scaledToFit()
-                                .scaleEffect(0.8))
+                                .scaleEffect(0.9))
                 Text("Americas")
                     .font(.callout)
             }
@@ -214,37 +215,77 @@ struct HighlightsSection: View {
                 Circle()
                     .stroke()
                     .foregroundColor(.gray)
-                    .frame(width: 70, height: 70)
-                    .overlay(Image(systemName: "globe.europe.africa")
-                                .resizable()
-                                .scaledToFit()
-                                .scaleEffect(0.8))
-                Text("Eu & Af")
-                    .font(.callout)
-            }
-            VStack {
-                Circle()
-                    .stroke()
-                    .foregroundColor(.gray)
-                    .frame(width: 70, height: 70)
-                    .overlay(Image(systemName: "globe.asia.australia")
-                                .resizable()
-                                .scaledToFit()
-                                .scaleEffect(0.8))
-                Text("Asia & Au")
-                    .font(.callout)
-            }
-            VStack {
-                Circle()
-                    .stroke()
-                    .foregroundColor(.gray)
-                    .frame(width: 70, height: 70)
+                    .frame(width: 60, height: 60)
                     .overlay(Image(systemName: "plus")
                                 .resizable()
                                 .scaledToFit()
                                 .scaleEffect(0.3))
                 Text("New")
                     .font(.callout)
+            }
+        }
+    }
+}
+
+struct TabBar: View {
+    @Binding var selection: Int
+    var body: some View {
+        HStack {
+            Spacer()
+            Button {
+                withAnimation(.linear) {
+                    selection = 0
+                }
+            } label: {
+                Image(systemName: "squareshape.split.3x3")
+                    .resizable()
+                    .scaledToFit()
+                    .scaleEffect(0.4)
+                    .tint(.white)
+            }
+            
+            .overlay( selection == 0 ?
+                      Rectangle()
+                        .frame(width: 100, height: 3) : nil, alignment: .bottom)
+            
+            Spacer()
+            Button {
+                withAnimation(.linear) {
+                    selection = 1
+                }
+                
+            } label: {
+                Image(systemName: "person.crop.rectangle")
+                    .resizable()
+                    .scaledToFit()
+                    .scaleEffect(0.4)
+                    .tint(.white)
+            }
+            .overlay( selection == 1 ?
+                      Rectangle()
+                        .frame(width: 100, height: 3) : nil, alignment: .bottom)
+            Spacer()
+        }
+        .frame(height: 50)
+    }
+}
+
+struct TabViewGrid: View {
+    @Binding var selection: Int
+    var columns: [GridItem] =
+    Array(repeating: .init(.flexible(minimum: 40), spacing: 1), count: 3)
+    var body: some View {
+        LazyVGrid(columns: columns, spacing: 1) {
+            ForEach(0..<54) { _ in
+                Rectangle()
+                    .foregroundColor(.black)
+                    .scaledToFit()
+                    .overlay(Image(systemName: selection == 0 ? "photo" : "person")
+                                .resizable()
+                                .scaledToFit()
+                                .scaleEffect(0.6)
+                                .foregroundColor(.gray))
+                    .animation(.linear, value: selection)
             }
         }
     }
