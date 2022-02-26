9d3ac7e72f5a829ad4e4211450a5a285a8e86d75
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-02-26 18:10:38 +0300

    Created individual story view in feed stories bar

---
+++ InstaClone/Views/FeedStoriesBar.swift
@@ -0,0 +1,20 @@
+//
+//  FeedStoriesBar.swift
+//  InstaClone
+//
+//  Created by MJ Albdour on 2/26/22.
+//
+
+import SwiftUI
+
+struct FeedStoriesBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedStoriesBar_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedStoriesBar()
+    }
+}
