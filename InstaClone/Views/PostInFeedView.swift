de27aa9961a348fd4625d96b301d6fcfea5b159e
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-02-27 10:03:20 +0300

    Added Post Caption

--- InstaClone/Views/PostInFeedView.swift
+++ InstaClone/Views/PostInFeedView.swift
@@ -14,6 +14,14 @@ struct PostInFeedView: View {
             Content()
             Footer()
             Likes()
+            VStack(alignment: .leading, spacing: 0) {
+                Text("some_username")
+                    .bold()
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                    .lineLimit(1)
+                    .truncationMode(.tail)
+            }
+            .padding(.horizontal)
         }
     }
 }
